8f895642a955e3f475ebc0f19b04e85939ffe05a
Brett Ambrose <brettambrose@outlook.com>
2025-04-30 19:31:46 -0400

    data quality operator first draft

--- plugins/operators/data_quality.py
+++ plugins/operators/data_quality.py
@@ -2,21 +2,47 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
-class DataQualityOperator(BaseOperator):
+"""
+TODO: Summary
 
+TODO: Args
+"""
+
+class DataQualityOperator(BaseOperator):
     ui_color = '#89DA59'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 rule_list=[],
                  *args, **kwargs):
 
         super(DataQualityOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.rule_list = rule_list
 
     def execute(self, context):
-        self.log.info('DataQualityOperator not implemented yet')
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info("executing data quality rules...")
+
+        for rule in self.rule_list:
+            id = rule.get("id")
+            rule_type = rule.get("type")
+            exp_result = rule.get("exp_result")
+            query = rule.get("query")
+
+            raw_result = redshift.get_records(query)
+            result = raw_result[0][0]
+
+            if rule_type == "condition":
+                if result != int(exp_result):
+                    raise ValueError(f"RULE {id} FAILED: exp result={exp_result} | actual result={result}")
+                else:
+                    self.log.info(f"RULE {id} PASSED: exp result={exp_result} | actual result={result}")
+            
+            if rule_type == "checksum":
+                if result <= 0:
+                    raise ValueError(f"RULE {id} FAILED: {query} returned 0 records")
+                else:
+                    self.log.info(f"RULE {id} PASSED: {query} returned > 0 records")
